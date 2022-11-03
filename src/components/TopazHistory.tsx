bc319c15f28665e3d6b1153fd81df7cff436a964
spuro <36612221+spuro@users.noreply.github.com>
2022-11-03 19:36:48 +1030

    'time ago' stamps on buys

--- src/components/TopazHistory.tsx
+++ src/components/TopazHistory.tsx
@@ -10,15 +10,62 @@ type Props = {
   addressResources: Types.MoveResource[] | null;
 };
 
+function timeAgo(timestampString) {
+  const timestampNumber = Number(timestampString / 1000);
+
+  const txTime: any = new Date(timestampNumber);
+  const secondsSince = (Date.now() - txTime) / 1000;
+
+  const minutes_per_second = 60;
+  const whole_minutes =
+    (secondsSince - (secondsSince % minutes_per_second)) / minutes_per_second;
+  const whole_seconds = secondsSince % minutes_per_second;
+
+  const minutes_per_hour = 60;
+  const whole_hours =
+    (whole_minutes - (whole_minutes % minutes_per_hour)) / minutes_per_hour;
+
+  const hours_per_day = 24;
+  const whole_days =
+    (whole_hours - (whole_hours % hours_per_day)) / hours_per_day;
+
+  const days_per_week = 7;
+  const whole_weeks =
+    (whole_days - (whole_days % days_per_week)) / days_per_week;
+
+  if (whole_minutes === 0) {
+    return `${whole_seconds.toFixed(0)} seconds ago`;
+  } else if (whole_hours === 0 && whole_days === 0) {
+    return `${whole_minutes} minutes and ${whole_seconds.toFixed(
+      0
+    )} seconds ago`;
+  } else if (whole_days === 0 && whole_weeks === 0) {
+    return `${whole_hours} hours and ${
+      whole_minutes - whole_hours * minutes_per_hour
+    } minutes ago`;
+  } else if (whole_weeks === 0) {
+    return `${whole_days} days and ${
+      whole_hours - whole_days * hours_per_day
+    } hours ago`;
+  } else {
+    return `${whole_weeks} weeks and ${
+      whole_days - whole_weeks * days_per_week
+    } days ago`;
+  }
+}
+
 function NFTHistory({ adapter, addressResources }: Props) {
   const [topazHistory, setTopazHistory] = useState([]);
 
   const fetchEvents = async () => {
-    const txHistory = await client.getEventsByEventHandle(
+    let txHistory = await client.getEventsByEventHandle(
       "0x2c7bccf7b31baf770fdbcc768d9e9cb3d87805e255355df5db32ac9a669010a2",
       "0x2c7bccf7b31baf770fdbcc768d9e9cb3d87805e255355df5db32ac9a669010a2::events::Events",
       "buy_events",
       { limit: 25 }
+    );
+    txHistory = txHistory.sort(
+      (a, b) => parseFloat(b.sequence_number) - parseFloat(a.sequence_number)
     );
     setTopazHistory(txHistory);
   };
@@ -86,6 +133,12 @@ const EventHistory = ({ event }) => {
           10 ** 8
         ).toFixed(2)} $APT`}</p>
       </div>
+      <p
+        style={{ gridColumn: "1/4" }}
+        className="w-full text-center text-sm text-teal-100"
+      >
+        {timeAgo(event.data.timestamp)}
+      </p>
     </div>
   );
 };
